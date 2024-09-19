251d7c7bac9f7d41a99adb0ba6e3f3ab2875785b
BRICE DENANKPO <denankpobrice@gmail.com>
2024-09-19 14:56:19 +0100

    Frontend améliroer

--- resources/views/Import.blade.php
+++ resources/views/Import.blade.php
@@ -9,31 +9,35 @@
   </head>
   <body>
     <div class="container mt-5">
-
-        <h1 class="mb-4">Importer votre fichier CSV, XLS ou autre</h1>
-
-        @if(session('success'))
-            <div class="alert alert-success" role="alert">
-                {{ session('success') }}
+        <div class="card">
+            <div class="card-header">
+                <h2>Importer votre fichier CSV, XLS ou autre</h2>
             </div>
-        @endif
+            <div class="card-body">
+                @if(session('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('success') }}
+                    </div>
+                @endif
 
-        @if(session('error'))
-            <div class="alert alert-danger" role="alert">
-                {{ session('error') }}
+                @if(session('error'))
+                    <div class="alert alert-danger" role="alert">
+                        {{ session('error') }}
+                    </div>
+                @endif
+
+                <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="file" class="form-label">Choisir un fichier</label>
+                        <input type="file" name="file" id="file" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Importer</button>
+                </form>
             </div>
-        @endif
-
-        <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
-                <label for="file" class="form-label">Choisir un fichier</label>
-                <input type="file" name="file" id="file" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Importer</button>
-        </form>
-
+        </div>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
