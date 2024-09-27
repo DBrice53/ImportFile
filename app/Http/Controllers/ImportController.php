032358bd048e054f4f05331df5ad3498c281fb3e
BRICE DENANKPO <denankpobrice@gmail.com>
2024-09-27 17:30:18 +0100

    Transaction atomique ajouté pour éviter les erreurs d'envoie de donnée

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -5,33 +5,47 @@ namespace App\Http\Controllers;
 use App\Imports\ImportFiles;
 use App\Models\Import;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; // Importation de la façade DB pour les transactions
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Redirect;
 
 class ImportController extends Controller
 {
-    public function index(){
-
+    // Affiche la page d'importation avec les données importées
+    public function index()
+    {
+        // Récupérer toutes les données de la table "imports"
         $importedData = Import::all();
-        return view('Import', ['imports' => $importedData]); // Assurez-vous que cette vue est correctement configurée
+
+        // Passer les données récupérées à la vue "Import"
+        return view('Import', ['imports' => $importedData]);
     }
 
-    public function import(Request $request){
-        // Validation du fichier
+    // Gère l'importation de fichiers Excel avec transaction atomique
+    public function import(Request $request)
+    {
+        // Validation du fichier téléchargé
         $request->validate([
             'file' => 'required|mimes:xls,xlsx,csv|max:2048',
         ]);
 
+        // Début de la transaction
+        DB::beginTransaction();
+
         try {
-            // Importation du fichier
+            // Importation du fichier Excel
             Excel::import(new ImportFiles, $request->file('file'));
 
+            // Confirme la transaction si tout est correct
+            DB::commit();
 
-
-            // Redirection avec message de succès
+            // Redirection avec un message de succès
             return Redirect::back()->with('success', 'Fichier importé avec succès.');
         } catch (\Exception $e) {
-            // Redirection avec message d'erreur en cas d'exception
+            // En cas d'erreur, annule la transaction
+            DB::rollBack();
+
+            // Redirection avec un message d'erreur
             return Redirect::back()->with('error', 'Une erreur est survenue lors de l\'importation du fichier.');
         }
     }
